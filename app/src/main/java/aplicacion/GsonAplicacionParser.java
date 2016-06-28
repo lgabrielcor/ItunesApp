3fc1cb76e58927d5c791eb65cb633b55b7381352
lgabrielcor <lgabrielcor@gmail.com>
2016-06-27 20:43:31 -0500

    se agrega el diseño de detalle de aplicacion en lista

---
+++ app/src/main/java/aplicacion/GsonAplicacionParser.java
@@ -0,0 +1,51 @@
+package aplicacion;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Aplicacion;
+
+/**
+ * Created by luisgabrielcorredorcombita on 24/06/16.
+ */
+public class GsonAplicacionParser {
+    public List readJsonStream(InputStream in) throws IOException {
+
+        Gson gson = new Gson();
+
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        List aplicaciones = new ArrayList();
+        reader.beginObject();
+
+        while (reader.hasNext()) {
+            //busca los subgeneros
+
+
+            String subgenres = reader.nextName();
+            if (subgenres.contains("subgenres") && !subgenres.isEmpty()) {
+                //recorre los objetos
+                while (reader.hasNext()) {
+                    Aplicacion aplicacion = new Aplicacion();
+                    //categoria.setCodigo(String.valueOf(reader.nextInt()));
+                    //categoria.setCodigo("0");
+                    //categoria.setNombre("prueba");
+                    aplicaciones.add(aplicacion);
+                }
+
+                //Categoria categoria = gson.fromJson(reader, Categoria.class);
+
+            }
+
+        }
+        reader.endArray();
+        reader.close();
+
+        return aplicaciones;
+    }
+}
---
+++ app/src/main/java/aplicacion/JsonServicioClienteAplicacion.java
@@ -0,0 +1,58 @@
+package aplicacion;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Aplicacion;
+
+
+/**
+ * Created by luisgabrielcorredorcombita on 24/06/16.
+ */
+public class JsonServicioClienteAplicacion extends AsyncTask<URL, Void, List<Aplicacion>> {
+
+    HttpURLConnection con = null;
+
+    @Override
+    protected List<Aplicacion> doInBackground(URL... urls) {
+        List aplicaciones = null;
+
+        // Establecer la conexión
+        try {
+            con = (HttpURLConnection)urls[0].openConnection();
+            con.setConnectTimeout(15000);
+            con.setReadTimeout(10000);
+
+            int statusCode = con.getResponseCode();
+
+            if(statusCode!= 200){
+                aplicaciones = new ArrayList();
+                aplicaciones.add(new Aplicacion());
+
+            }else{
+
+                InputStream in = new BufferedInputStream(con.getInputStream());
+
+                //llamar el parser
+                GsonAplicacionParser parser = new GsonAplicacionParser();
+
+                aplicaciones = parser.readJsonStream(in);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            con.disconnect();
+        }
+
+
+        return null;
+    }
+}

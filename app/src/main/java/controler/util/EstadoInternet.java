094703dbb968a4d63e964c4afc47678d0075f5a0
lgabrielcor <lgabrielcor@gmail.com>
2016-06-27 23:25:47 -0500

    Adiciona verificacion de Internet

--- app/src/main/java/controler/util/EstadoInternet.java
+++ app/src/main/java/controler/util/EstadoInternet.java
@@ -5,7 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 /**
- * Created by luisgabrielcorredorcombita on 27/06/16.
+ * Created by luisgabrielcorredorcombita on 27/06/16
  */
 public class EstadoInternet {
     static public boolean isOnline(Context context) {

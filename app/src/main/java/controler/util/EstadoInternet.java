bc2d0114de81813e7045286be50b8df0307d09c5
lgabrielcor <lgabrielcor@gmail.com>
2016-06-27 23:24:56 -0500

    Se adiciona Crud de cache

---
+++ app/src/main/java/controler/util/EstadoInternet.java
@@ -0,0 +1,22 @@
+package controler.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by luisgabrielcorredorcombita on 27/06/16.
+ */
+public class EstadoInternet {
+    static public boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+
+        return false;
+    }
+}
